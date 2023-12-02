05c00e58d2ee5c4388b6effe72ea1fdc7b8bdbdf
Minhajur Rahman Siam <minhajsiam007@gmail.com>
2023-12-02 12:42:23 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ let inputFile = document.getElementById("input-file");
 inputFile.onchange = function () {
     profilePic.src = URL.createObjectURL(inputFile.files[0]);
 }
+
+
+import React, { useState } from 'react';
+
+const ProfilePicUploader = () => {
+    const [profilePicSrc, setProfilePicSrc] = useState('');
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+
+        if (file) {
+            setProfilePicSrc(URL.createObjectURL(file));
+        }
+    };
+
+    return (
+        <div>
+            <img id="profile-pic" src={profilePicSrc} alt="Profile" />
+            <input type="file" id="input-file" onChange={handleFileChange} />
+        </div>
+    );
+};
+
+export default ProfilePicUploader;
